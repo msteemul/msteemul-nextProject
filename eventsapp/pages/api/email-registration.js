7829c268a51668aca2e55b02ab0f5fb9c7884fda
msteemul <113412804+msteemul@users.noreply.github.com>
2023-01-28 16:12:03 +0400

    api routing

--- eventsapp/pages/api/email-registration.js
+++ eventsapp/pages/api/email-registration.js
@@ -1,9 +1,62 @@
+import path from 'path';
+import fs from 'fs'; //allow us to read and overite data of a file
+
+//path built so we can reach our db in this case data.json file
+function buildPath() {
+  return path.join(process.cwd(), 'data', 'data.json');
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export default function handler(req, res) {
-  const { email } = req.body;
-  if (!email) {
-    res.status(422).json({ message: 'Invalid email address.' });
-    return;
+  const { method } = req;
+
+  //we need to acces our data{json file}, extract it
+  //response 404 if no events
+  //extract {All_events}, look through them and identify event id
+  //then add the email to it in emils_registered -write functionto write our data
+  //if email doesn't exist we check format of email
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
+  //check if all events has data
+  if (!allEvents) {
+    return res.status(404).json({
+      status: 404,
+      message: 'Events data not found',
+    });
   }
-  // Store it in a database or send an email
-  res.status(201).json({ message: 'Signed up!' });
+
+  if (method === 'POST') {
+    const { email, eventId } = req.body;
+
+    if (!email | !email.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address' });
+    }
+
+    const newAllEvents = allEvents.map((ev) => {
+      if (ev.id === eventId) {
+        if (ev.emails_registered.includes(email)) {
+          res.status(409).json({ message: 'This email has already been registered' });
+          return ev;
+        }
+        return {
+          ...ev,
+          emails_registered: [...ev.emails_registered, email],
+        };
+      }
+      return ev;
+    });
+
+    fs.writeFileSync(filePath, JSON.stringify({ events_categories, allEvents: newAllEvents }));
+
+    res.status(201).json({
+      message: `You have been registered successfully with the email: ${email} for the event: ${eventId}`,
+    });
+  }
 }
--- eventsapp/src/components/home/home-page.jsx
+++ eventsapp/src/components/home/home-page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const HomePage = ({ data }) => (
   <div className="home_body">
     {data?.map((e) => (
-      <Link class="card" key={e.id} href={`/events/${e.id}`}>
+      <Link className="card" key={e.id} href={`/events/${e.id}`}>
         <div className="image">
           <Image width={600} height={400} alt={e.title} src={e.image} />
         </div>
